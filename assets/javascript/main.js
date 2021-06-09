04e222801fa1ad39d994a58410a792877db48517
DESKTOP-KMNS09Q\Admin <quangtuankc040999@gmail.com>
2021-06-09 10:59:40 +0700

    + fix download

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -53,30 +53,31 @@ function scrollTop() {
 window.addEventListener('scroll', scrollTop)
 
 /*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
 
-/*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
 
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+}
 
-/*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
-
-
-/*==================== GENERATE PDF ====================*/
-// PDF generated area
-
-
-// Html2pdf options
-
-
-// Function to call areaCv and Html2Pdf options 
-
-
-// When the button is clicked, it executes the three functions
-
-// 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
-
-// 2. The PDF is generated
-
-
-// 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+        // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
